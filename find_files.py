a3cf0b7db9bc713fdabb915edb33803236b18ae7
mikucy <419714107@qq.com>
2017-06-06 14:53:43 +0800

    Update

--- find_files.py
+++ find_files.py
@@ -1,5 +1,7 @@
 import os
 
+# This function find all the files in a directory including its subfolders according to a keyword.
+
 def find_files(dir='/home/cp612sh', keyword='Python'):
   for i in os.walk(dir):
     for j in i[2]:
